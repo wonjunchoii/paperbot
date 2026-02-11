e7835ed1dd593170a453f0bb4fef40b7c6b646ad
wonjuncio <rich4103@naver.com>
2026-02-11 15:24:17 +0900

    entry point 안정화

--- paperbot/ui.py
+++ paperbot/ui.py
@@ -2,20 +2,22 @@ import uvicorn
 import threading
 import webbrowser
 
+from paperbot.gui.app import app
+
 def _open_app_window(url: str) -> None:
     """Open the app window."""
     webbrowser.open(url)
     
 def run_ui() -> None:
     """Main entry point for UI."""
-    host, port = "127.0.0.1", 8000
+    host, port = "127.0.0.1", 8001
     url = f"http://{host}:{port}"
 
     threading.Timer(0.8, lambda: _open_app_window(url)).start()
     uvicorn.run(
-        "paperbot.gui.app:app",
+        app,
         host=host,
         port=port,
-        reload=True,
+        reload=False,
         access_log=False
     )
